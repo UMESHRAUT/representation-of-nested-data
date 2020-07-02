c691d5bb7b7c11eb8068c73c0d70837332c4919a
UMESHRAUT <rautumesh34@gmail.com>
2020-07-02 15:24:54 +0530

    Updates

---
+++ precache-manifest.227482458f75a92632b0231f4c7f39c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "957a480e9773fdcb2357bf2ca52b8d81",
+    "url": "/representation-of-nested-data/index.html"
+  },
+  {
+    "revision": "30c90bb4dfcc5f0a8a93",
+    "url": "/representation-of-nested-data/static/css/main.07ded741.chunk.css"
+  },
+  {
+    "revision": "da2e4cafaaa94c8c3503",
+    "url": "/representation-of-nested-data/static/js/2.bb92c620.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/representation-of-nested-data/static/js/2.bb92c620.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30c90bb4dfcc5f0a8a93",
+    "url": "/representation-of-nested-data/static/js/main.59f930a6.chunk.js"
+  },
+  {
+    "revision": "3654cd88fbfc7600b0ff",
+    "url": "/representation-of-nested-data/static/js/runtime-main.0c994b55.js"
+  }
+]);
